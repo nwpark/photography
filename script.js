77f67d31ae291820f74968ca19f1ffe461518eef
nwpark <nick.wpa@gmail.com>
2015-03-27 17:13:18 +0000

    changes

--- script.js
+++ script.js
@@ -5,49 +5,117 @@ var futurismPics = ["futurism/SECOND CUP.jpg",
 			"futurism/hananh.jpg"]
 var flatsPics = ["flats/10574490_916149055066311_9166328785546283049_n.jpg",
 			"flats/10570496_916149141732969_7109096628975262258_n.jpg",
-			"flats/10551061_916148528399697_2928115284938901327_n.jpg",]
+			"flats/10551061_916148528399697_2928115284938901327_n.jpg",
+			"flats/10527332_916148831733000_6703671114500597545_n.jpg",
+			"flats/10418909_916147995066417_5982149672785673479_n.jpg",
+			"flats/10392409_916148698399680_5145691371804763473_n.jpg",
+			"flats/10377164_916148838399666_7791326558864167979_n.jpg",
+			"flats/10355722_916148251733058_3549712943006763498_n.jpg",
+			"flats/1458707_916148071733076_7972319385168546822_n.jpg",
+			"flats/995614_916148801733003_3320039399786898288_n.jpg",
+			"flats/7357_916149121732971_4074852418261063885_n.jpg"]
+var london_fashion_weekPics = ["london_fashion_week/picture30.jpg",
+				"london_fashion_week/picture26.jpg",
+				"london_fashion_week/picture24.jpg",
+				"london_fashion_week/picture20.jpg",
+				"london_fashion_week/picture13.jpg",
+				"london_fashion_week/picture6.jpg",
+				"london_fashion_week/picture4.jpg",
+				"london_fashion_week/picture2.jpg"]
 var i = 0;
+var currentSrc = "futurism";
 function hideLargeImage() {
-	var imageVar = document.getElementById('image');
-	imageVar.style.display = 'none';
+	document.getElementById('image').style.display = 'none';
+	document.getElementById('box').style.display = 'block';
+	document.getElementById('lowerBox').style.display = 'block';
+	document.getElementById('boxFooter').style.display = 'block';
 }
 function showLargeImage() {
-	var largeImageDiv = document.getElementById('image');
-	var picture = document.getElementById('mainImage');
-	var largePicture = document.getElementById('largeImage');
-	largePicture.src = picture.src;
-	largeImageDiv.style.display = 'block';
-	largeImageDiv.style.position = 'fixed';
-	largeImageDiv.style.top = '0px';
-	largeImageDiv.style.left = '10%';
+	document.getElementById('largeImage').src = document.getElementById('mainImage').src;
+	document.getElementById('image').style.display = 'block';
+	document.getElementById('box').style.display = 'none';
+	document.getElementById('lowerBox').style.display = 'none';
+	document.getElementById('boxFooter').style.display = 'none';
+}
+function hideThumbnails() {
+	var thumbnailStatus = document.getElementById('lowerBox').style.display;
+	if(thumbnailStatus != 'none') {
+		document.getElementById('lowerBox').style.display = 'none';
+	}
+	if(thumbnailStatus == 'none') {
+		document.getElementById('lowerBox').style.display = 'block';
+	}
+}
+function showThumbnails() {
+	document.getElementById('lowerBox').style.display = 'block';
 }
 function nextImage() {
 	var picture = document.getElementById('mainImage');
 	i++;
-	if(futurismPics[i] != null) {
+	if(picture.src.indexOf("futurism") > -1) {
+		if(futurismPics[i] == null) {
+			i=0;
+		}
 		picture.src = futurismPics[i];
 	}
-	else {
-		i = 0;
-	}
-}
-function nextFlats() {
-	var picture = document.getElementById('flatsImage');
-	i++;
-	if(flatsPics[i] != null) {
+	if(picture.src.indexOf("flats") > -1) {
+		if(flatsPics[i] == null) {
+			i=0;
+		}
 		picture.src = flatsPics[i];
 	}
-	else {
-		i = 0;
+	if(picture.src.indexOf("london_fashion_week") > -1) {
+		if(london_fashion_weekPics[i] == null) {
+			i=0;
+		}
+		picture.src = london_fashion_weekPics[i];
 	}
+	for(j=0; j<50; j++) {
+		if(document.getElementById('image'+j) != null) {
+			document.getElementById('image'+j).style.opacity="";
+		}
+	}
+	document.getElementById('image'+i).style.opacity="1";
 }
 function prevImage() {
 	var picture = document.getElementById('mainImage');
 	i--;
-	if(futurismPics[i] != null) {
+	if(picture.src.indexOf("futurism") > -1) {
+		if(futurismPics[i] == null) {
+			i = futurismPics.length -1;
+		}
 		picture.src = futurismPics[i];
 	}
-	else {
-		i = futurismPics.length;
+	if(picture.src.indexOf("flats") > -1) {
+		if(flatsPics[i] == null) {
+			i = flatsPics.length -1;
+		}
+		picture.src = flatsPics[i];
 	}
+	if(picture.src.indexOf("london_fashion_week") > -1) {
+		if(london_fashion_weekPics[i] == null) {
+			i = london_fashion_weekPics.length -1;
+		}
+		picture.src = london_fashion_weekPics[i];
+	}
+	for(j=0; j<50; j++) {
+		if(document.getElementById('image'+j) != null) {
+			document.getElementById('image'+j).style.opacity="";
+		}
+	}
+	document.getElementById('image'+i).style.opacity="1";
+}
+function changeMainImage(newImage) {
+	i = newImage -1;
+	nextImage();
+	for(j=0; j<50; j++) {
+		if(document.getElementById('image'+j) != null) {
+			document.getElementById('image'+j).style.opacity="";
+		}
+	}
+	document.getElementById('image'+newImage).style.opacity="1";
+}
+function changeSrc(newSrc) {
+	document.getElementById('content').src = newSrc+".html";
+	i=0;
 }
